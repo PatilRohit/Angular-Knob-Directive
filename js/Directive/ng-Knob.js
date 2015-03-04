4c3204c3bfe760a1c3148b0a4986dc8ee41a10df
Rohit Patil <rohitpa@futurismtechnologies.com>
2015-03-04 21:06:25 +0530

    Initial Commit

---
+++ js/Directive/ng-Knob.js
@@ -0,0 +1,34 @@
+
+angular.module('ng-Knob',[])
+.directive('ngKnob',['$timeout',function($timeout){
+		return {
+			restrict:'E',
+		    require: 'ngModel',
+			scope :{
+				id: '@',
+				value : '=ngModel'
+			}	,	
+			link:function(scope,element,attr,cntrl){
+				debugger;
+				var tmplt= '<input type="text"  value='+scope.value+' id="'+scope.id+'i" >';
+				var _knob = jQuery(tmplt)
+						   .appendTo(element)
+						   .knob({
+                           'min':0,
+                           'max':100,
+                           'step':1,
+                           'release':function(v){
+                           	   scope.value=v;
+                           }
+                           });      
+                 //$('#hh').val(scope.value).trigger('change');
+                $timeout(function () {
+			    scope.$watch('value', function (updatedValue) {
+                      if (updatedValue !== undefined) {
+                        $('#'+scope.id+'i').val(updatedValue).trigger('change');
+                   }
+                }, true);});
+			
+			}
+		}
+	}]) ;
---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+angular.module('ngKnobExample',['ng-Knob'])
+	.controller('knobCnt',function($scope){
+          
+        //   $('#hh').val(50).trigger('change'); 
+          $scope.change=20;
+          $scope.changec=30;
+          $scope.changer=90;
+
+	});
+	
